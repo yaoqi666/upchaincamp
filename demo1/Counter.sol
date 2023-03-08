683db92c597e248c649b287d8bb96bc6a32d728f
kpyaoqi <123849103+kpyaoqi@users.noreply.github.com>
2023-03-08 14:44:13 +0800

    Update Counter.sol

--- demo1/Counter.sol
+++ demo1/Counter.sol
@@ -5,6 +5,7 @@ contract Counter {
     uint256 public counter;
     // 默认值
     uint256 private init;
+
     // Log事件
     event Log(string funName, address from, uint256 value, bytes data);
 
@@ -16,13 +17,25 @@ contract Counter {
 
     // 传入值与counter相加并复制给counter
     function add(uint256 x) public {
+        // require((counter + x) <= type(uint256).max, unicode"相加后超过最大值");
         require((type(uint256).max - counter) > x, unicode"相加后超过最大值");
+
         counter += x;
     }
 
     // 恢复默认值
     function defaultCounter() public {
         counter = init;
+    }
+
+    // 查询地址余额
+    function balanceOf(address addr) public view returns (uint256) {
+        return addr.balance;
+    }
+
+    // 查询当前合约余额
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
     }
 
     fallback() external {
