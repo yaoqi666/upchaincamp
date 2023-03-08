b91d5066a8f8fdabc6419ebd9b53818e18b05a9e
kpyaoqi <yaoqi2136712851@gmail.com>
2023-03-08 08:42:00 +0800

    第一次作业

--- demo1/Counter.sol
+++ demo1/Counter.sol
@@ -6,6 +6,8 @@ contract Counter {
     // 默认值
     uint256 private init;
 
+    event Log(string funName, address from, uint256 value, bytes data);
+
     // 传入初始值
     constructor(uint256 num) {
         counter = num;
@@ -14,12 +16,22 @@ contract Counter {
 
     // 传入值与counter相加并复制给counter
     function add(uint256 x) public {
-        require((counter + x) <= type(uint256).max, unicode"相加后超过最大值");
+        // require((counter + x) <= type(uint256).max, unicode"相加后超过最大值");
+        require((type(uint256).max - counter) > x, unicode"相加后超过最大值");
+
         counter += x;
     }
 
     // 恢复默认值
     function defaultCounter() public {
         counter = init;
+    }
+
+    fallback() external {
+        emit Log("fallback", msg.sender, 0, msg.data);
+    }
+
+    receive() external payable {
+        emit Log("receive", msg.sender, msg.value, "");
     }
 }
