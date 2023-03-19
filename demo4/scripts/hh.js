0eb7df65dc1c95537d3ac9d84e16b9b94f5f7dbb
kpyaoqi <yaoqi2136712851@gmail.com>
2023-03-19 21:17:45 +0800

    第四次作业

---
+++ demo4/scripts/hh.js
@@ -0,0 +1,5 @@
+const web3=require("web3");
+var Chinese=web3.utils.asciiToHex("Chinese");
+var Math=web3.utils.asciiToHex("Math");
+var English=web3.utils.asciiToHex("English");
+console.log(Chinese,Math,English);
